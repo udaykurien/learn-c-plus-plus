14df28b90fbbec62120ab7c2981eef64a00af5dd
UK <uday.kurien@gmail.com>
2022-11-25 04:48:02 -0500

    1

--- Source/08-pointers_pointer_to_char.cpp
+++ Source/08-pointers_pointer_to_char.cpp
@@ -21,7 +21,7 @@ int main() {
    std::cout << "The address contained in pointer p_char_var is: " << p_char_var << ". And the value contained in p_char_var: " << *p_char_var << std::endl;
    std::cout << std::endl;
 
-   char *p_message {"Hello World"}; // Pointer points to first element of char array that represents the string. In this case its H.
+   //char *p_message {"Hello World"}; // Pointer points to first element of char array that represents the string. In this case its H.
    // some versioins of c++ will try to expand the literal as an array of constant characters (const char)
    // in that case there will be a type mis-match between the char array type (const char) and the pointer type (char).
    // Because of this mismatch we'll get a compiler error
@@ -31,7 +31,7 @@ int main() {
    // to avoid the error, perhaps the pointer type (const char) and char array type (const char) should match.
    // Modifyign the pointer type to be a const char pointer will prevent us from accidentally trying to modify it, and this will satisfy the compiler.
    
-   std::cout << p_message << " , " << *p_message << std::endl;
+   //std::cout << p_message << " , " << *p_message << std::endl;
    // p_message is a pointer, but cout << p_message will still print the literal
    // *p_message will print the content of the pointer ie. the first char in the literal.
    // Because p_message points at the first element of Hello World!, i.e. H, H will be printed when cout << *p_message is encountered.
